5374187972b89dbadd50b18a357c7f17735d8ec6
wahuu2 <lilywahu1@gmail.com>
2026-07-21 13:07:28 +0300

    Creating constants.ts and updating EventCard Component

--- app/page.tsx
+++ app/page.tsx
@@ -1,30 +1,29 @@
 import EventCard from "@/components/EventCard";
 import ExploreBtn from "@/components/ExploreBtn";
-
-const events = [
-  { image: "/images/event1.png", title: "Event 1" },
-  { image: "/images/event2.png", title: "Event 2" },
-];
+import { events } from "@/lib/constants";
 
 const Home = () => {
   return (
     <section className="px-6 py-12">
-      <h1 className="text-center text-3xl font-bold">
-        The Hub for Every Dev <br /> Event You Can't Miss
-      </h1>
-      <p className="text-center mt-5 text-gray-600">
-        Hackathons, Meetups, and Conferences All in One Place
-      </p>
+      {/* Hero Section */}
+      <header className="text-center">
+        <h1 className="text-3xl font-bold leading-tight">
+          The Hub for Every Dev <br /> Event You Can't Miss
+        </h1>
+        <p className="mt-5 text-gray-600">
+          Hackathons, Meetups, and Conferences All in One Place
+        </p>
+        <div className="flex justify-center mt-8">
+          <ExploreBtn />
+        </div>
+      </header>
 
-      <div className="flex justify-center mt-8">
-        <ExploreBtn />
-      </div>
-
+      {/* Featured Events */}
       <div className="mt-20 space-y-7">
         <h3 className="text-xl font-semibold">Featured Events</h3>
-        <ul className="space-y-6">
-          {events.map((event, index) => (
-            <li key={index}>
+        <ul className="grid gap-6 sm:grid-cols-2 lg:grid-cols-3">
+          {events.map((event) => (
+            <li key={event.slug}>
               <EventCard {...event} />
             </li>
           ))}
