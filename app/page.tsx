95a22cfa4043bd875c6a104062d0779aa00348cb
wahuu2 <lilywahu1@gmail.com>
2026-07-21 12:10:07 +0300

    Creating the public and Explore Button Component

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,11 @@
+import ExploreBtn from "@/components/ExploreBtn"
+
 const Home = () => {
   return (
     <section>
       <h1 className="text-center">The Hub for Every Dev <br/>Event You Can't Miss</h1>
       <p className="text-center mt-5">Hackathons,Meetups, and Conferences All in One Place</p>
+      <ExploreBtn/>
     </section>
   )
 }
