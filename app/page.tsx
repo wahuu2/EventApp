c06ec52a75db89bf701518567343e52945d99662
wahuu2 <lilywahu1@gmail.com>
2026-07-21 12:40:07 +0300

    Creating EventCard Component

--- app/page.tsx
+++ app/page.tsx
@@ -1,28 +1,37 @@
-import EventCard from "@/components/EventCard"
-import ExploreBtn from "@/components/ExploreBtn"
+import EventCard from "@/components/EventCard";
+import ExploreBtn from "@/components/ExploreBtn";
 
-const events=[
-  {image:'/images/event1.png',title:'Event 1'},
-  {image:'/images/event2.png',title:'Event 2'},
-]
+const events = [
+  { image: "/images/event1.png", title: "Event 1" },
+  { image: "/images/event2.png", title: "Event 2" },
+];
+
 const Home = () => {
   return (
-    <section>
-      <h1 className="text-center">The Hub for Every Dev <br/>Event You Can't Miss</h1>
-      <p className="text-center mt-5">Hackathons,Meetups, and Conferences All in One Place</p>
-      <ExploreBtn/>
+    <section className="px-6 py-12">
+      <h1 className="text-center text-3xl font-bold">
+        The Hub for Every Dev <br /> Event You Can't Miss
+      </h1>
+      <p className="text-center mt-5 text-gray-600">
+        Hackathons, Meetups, and Conferences All in One Place
+      </p>
+
+      <div className="flex justify-center mt-8">
+        <ExploreBtn />
+      </div>
+
       <div className="mt-20 space-y-7">
-        <h3>Featured Events</h3>
-        <ul className="events">
-          {[1,2,3,4,5].map((event)=>(
-            <li key={event.title}>
-              <EventCard{...event}/>
-              </li>
+        <h3 className="text-xl font-semibold">Featured Events</h3>
+        <ul className="space-y-6">
+          {events.map((event, index) => (
+            <li key={index}>
+              <EventCard {...event} />
+            </li>
           ))}
         </ul>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
