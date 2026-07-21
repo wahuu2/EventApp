986b2502b87eee9a8e2f5feb621f688850a368c6
wahuu2 <lilywahu1@gmail.com>
2026-07-21 11:53:58 +0300

    updates

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ const Home = () => {
   return (
     <section>
       <h1 className="text-center">The Hub for Every Dev <br/>Event You Can't Miss</h1>
-      <p>Hackathons,Meetups, and Conferences All in One Place</p>
+      <p className="text-center mt-5">Hackathons,Meetups, and Conferences All in One Place</p>
     </section>
   )
 }
